3a015c5ec6557ec5c779d46380d2f8094ba9891f
Kara <144894872+wongkara@users.noreply.github.com>
2024-09-05 11:13:52 -0400

    Update index.js

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -50,18 +50,3 @@ preloadImages();
 setupCanvas();
 drawFrame();
 setupLinkControl();
-
-button.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.volume = 0.2;
-    audio.play();
-    icon.classList.remove('fa-volume-up');
-    icon.classList.add('fa-volume-mute');
-    
-  } else {
-    audio.pause();
-    icon.classList.remove('fa-volume-mute');
-    icon.classList.add('fa-volume-up');
-  }
-  button.classList.add("fade");
-});
